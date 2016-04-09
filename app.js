6b01cf01196a195ca0618dcfbe09069e20bac96a
Laura <lg3295@gmail.com>
2016-04-08 20:24:42 -1000

    works on put method

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ var buzzObj = [];
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended : true }))
 
-app.get('/buzzwords', function(res){
+app.get('/buzzwords', function(req, res){
   res.json({ buzzWords : buzzObj});
 });
 
-app.post('/buzzwords', urlencodedParser, function(req, res){
+app.post('/buzzword', urlencodedParser, function(req, res){
   if (!req.body){
     return res.sendStatus(400);
   }
@@ -20,8 +20,31 @@ app.post('/buzzwords', urlencodedParser, function(req, res){
     req.body.hasOwnProperty('score')){
     req.body.score = Number(req.body.score);
     buzzObj.push(req.body);
+    return res.send({ "success" : true })
+  }
+  else {
+    return res.send({ "success" : false })
   }
 });
+
+app.put('/buzzwords', urlencodedParser, function(req, res){
+  if (!req.body){
+    return res.sendStatus(400);
+  }
+  buzzObj.forEach(function(){
+    if ((buzzObj.buzzword == req.body.buzzword) &&
+      req.body.hasOwnProperty('heard')){
+      buzzObj.heard = Boolean(req.body.heard);
+      return res.send({ "success" : true, "newScore" : buzzObj.score });
+    }
+    // else {
+    //   return res.send({ "success" : false });
+    // }
+
+  }); //ends FOR EACH
+  // }
+});
+
 
 var server = app.listen(8080, function(){
   console.log("App Listening!");
