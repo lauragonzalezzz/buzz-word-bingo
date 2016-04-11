eab69f479a793c84c1e687c40e6e351c0ccbb572
Laura <lg3295@gmail.com>
2016-04-11 12:25:58 -1000

    pushes all requests through validation.js as middleware to check that all requirements are met

--- validation.js
+++ validation.js
@@ -1,47 +1,14 @@
-// var validation = function(req, res, next){
-//   if (req.method === 'POST' && req.path === '/buzzword'){
-//     if (typeof req.body.buzzword === 'string' && req.body.hasOwnProperty('points')){
-//       return next();
-//       }
-//     }
-//   if (req.method === 'PUT' && req.path === '/buzzword'){
-//     if (req.body.hasOwnProperty('buzzword') && req.body.hasOwnProperty('heard')){
-//       return next();
-//     };
-//   }
-//   if(req.method === 'DELETE' && req.path === '/buzzword'){
-//     if (req.body.hasOwnProperty('buzzword')){
-//       return next();
-//     }
-//   }
-//   if (req.method === 'POST' && req.path === '/reset'){
-//     if (req.body.hasOwnProperty('reset')){
-//       return next();
-//     }
-//   }
-//   return res.send({ "success" : false });
-// };
-
-
-var validation = function(buzzObj, array){
+var validation = function(array){
   return function(req, res, next){
-    array.every(function(key){
-          console.log(key);
-          console.log('buzzObj',buzzObj);
-      for (var i = 0; i < buzzObj.length; i++){
-        if (buzzObj.hasOwnProperty(key)){
-          console.log("key2", key)
-          continue;
-        }
-
-        // else {
-        //   return res.send({ "success" : false });
-        // }
-      }
-      return;
+    var missingReqs = array.filter(function(key){
+      return !req.body.hasOwnProperty(key) || req.body[key].length === 0;
     });
-    console.log('done');
-    next();
+    if (missingReqs.length !== 0){
+      return res.sendStatus(400);
+    }
+    else {
+      next();
+    }
   } //End of middleware function
 } //End of Val
 module.exports = validation;
