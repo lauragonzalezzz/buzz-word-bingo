60445e908e8ba6ac77643ee0ae40fd18baa58b52
Laura <lg3295@gmail.com>
2016-04-11 12:25:18 -1000

    performs light validation on value types

--- app.js
+++ app.js
@@ -9,27 +9,29 @@ var buzzObj = [];
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended : true }))
-// app.use(function(req, res, next){
-//   return validation(req, res, next, buzzObj);
-// });
 
 app.get('/buzzwords',function(req, res){
   res.json({ buzzWords : buzzObj});
 });
 
-app.post('/buzzword', validation(buzzObj, ['buzzword', 'points']),function(req, res){
+app.post('/buzzword', validation(['buzzword', 'points']),function(req, res){
   if (buzzObj.length > 4){
     return res.status(400).send('Please remove a word before adding more, the limit is 5 words.')
   };
+  if (isNaN(Number(req.body.points))){
+    return res.status(400).send('Please enter a valid numerical points value');
+  }
   req.body.points = Number(req.body.points);
   buzzObj.push(req.body);
-  console.log(buzzObj);
   return res.send({ "success" : true })
 });
 
-app.put('/buzzword', validation(buzzObj, ['buzzword', 'heard']), function(req, res){
+app.put('/buzzword', validation(['buzzword', 'heard']), function(req, res){
   for (var i = 0; i < buzzObj.length; i++){
     if (buzzObj[i].buzzword == req.body.buzzword){
+      if (req.body.heard === true || req.body.heard === false){
+        return res.status(400).send('Please enter a valid boolean value for "heard"');
+      }
       buzzObj[i].heard = Boolean(req.body.heard);
       score += buzzObj[i].points;
       return res.send({ "success" : true, "newScore" : score });
@@ -38,7 +40,7 @@ app.put('/buzzword', validation(buzzObj, ['buzzword', 'heard']), function(req, r
   return res.send({ "success" : false });
 });
 
-app.delete('/buzzword', function(req, res){
+app.delete('/buzzword', validation(['buzzword']), function(req, res){
   for (var i = 0; i < buzzObj.length; i++){
     if (buzzObj[i].buzzword === req.body.buzzword){
       var index = i;
