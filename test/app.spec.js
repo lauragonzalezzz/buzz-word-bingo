9f3492367d26655b8f1727d3bbe5c90c2fc3fc83
Laura <lg3295@gmail.com>
2016-04-11 19:08:28 -1000

    tests that a correct post will be stored in memory

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,14 +3,6 @@ var app = require("../app.js");
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended : true }))
 
-// describe('GET /', function(){
-//   it('should pull up the index file', function(){
-//     request(app)
-//       .get('/')
-//       .expect("?")
-//   });
-// });
-
 describe('GET /buzzwords', function(){
 
   it('respond with json object', function(done){
@@ -30,16 +22,32 @@ describe('GET /buzzword', function(){
 
 describe('POST /buzzword', function(){
 
+  it('should save buzzword to memory', function(done){
+    var body = {'buzzword' : 'scala', 'points' : '2'};
+    request(app)
+    .post('/buzzword')
+    .type('form')
+    .send(body)
+    .expect(200, { 'success' : true }, function(err, res){
+      if (err){
+        return done(err);
+      }
+      request(app)
+      .get('/buzzwords')
+      .expect(200, { "buzzWords" : [{'buzzword' : 'scala', 'points' : '2'}]}, done)
+    });
+  });
+
   it('should return a success message', function(done){
     var body = {'buzzword' : 'scala', 'points' : '2'};
     request(app)
     .post('/buzzword')
     .type('form')
     .send(body)
-    .expect(200, done);
+    .expect(200, { 'success' : true }, done);
   });
 
-  it('should return an error message', function(done){
+  it('should return an error message upon incorrect data', function(done){
     var body2 = {'buzzword' : 'elm', 'score' : 'five'};
     request(app)
     .post('/buzzword')
@@ -47,4 +55,20 @@ describe('POST /buzzword', function(){
     .send(body2)
     .expect(400, done);
   });
+
+describe('PUT /buzzword', function(){
+
+  it('should return an error', function(done){
+    var body4 = {'buzzword' : 'elm', 'hapi' : 'five'};
+    request(app)
+    .put('/buzzword')
+    .type('form')
+    .send(body4)
+    .expect(400, done);
+  });
+
+
+
+});
+
 });
